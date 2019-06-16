5d3880056bcc3a572b65ada1aa9d07649ee17b73
Junhao Wang <junhaowww@outlook.com>
2019-06-16 10:32:41 -0700

    4.2 SwapBits - better

--- epi_judge_java/epi/SwapBits.java
+++ epi_judge_java/epi/SwapBits.java
@@ -4,19 +4,13 @@ import epi.test_framework.GenericTest;
 public class SwapBits {
   @EpiTest(testDataFile = "swap_bits.tsv")
   public static long swapBits(long x, int i, int j) {
-    if (i < 0 || j < 0 || i > 63 || j > 63) throw new IllegalArgumentException();
-
-    long imask = 1L << i; tobin(imask);
-    long jmask = 1L << j; tobin(jmask);
-
-    long ival = x & imask; // >0: 1, =0: 0
-    long jval = x & jmask;
-                               // ^= toggle a bit
-    if (jval > 0) x |= imask;  // |= turn on a bit
-    else          x &= ~imask; // &= turn off a bit
-    if (ival > 0) x |= jmask;
-    else          x &= ~jmask;
-
+    // Extract the i-th and j-th bits, and see if they differ
+    if (((x >>> i) & 1) != ((x >>> j) & 1)) { // diff
+      x ^= (1L << i);
+      x ^= (1L << j);
+      // or
+      // x ^= (1L << i) | (1L << j);
+    }
     return x;
   }
 
@@ -31,4 +25,24 @@ public class SwapBits {
                                 new Object() {}.getClass().getEnclosingClass())
             .ordinal());
   }
+
+  /*
+  // brute-force
+  public static long swapBits(long x, int i, int j) {
+    if (i < 0 || j < 0 || i > 63 || j > 63) throw new IllegalArgumentException();
+
+    long imask = 1L << i;
+    long jmask = 1L << j;
+
+    long ival = x & imask; // >0: 1, =0: 0
+    long jval = x & jmask;
+    // ^= toggle a bit
+    if (jval > 0) x |= imask;  // |= turn on a bit
+    else          x &= ~imask; // &= turn off a bit
+    if (ival > 0) x |= jmask;
+    else          x &= ~jmask;
+
+    return x;
+  }
+   */
 }
