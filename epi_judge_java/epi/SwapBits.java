c87aaac101c84ca496fbeab189a182675c29c470
Junhao Wang <junhaowww@outlook.com>
2019-06-16 01:25:01 -0700

    4.2 SwapBits - brute-force

--- epi_judge_java/epi/SwapBits.java
+++ epi_judge_java/epi/SwapBits.java
@@ -4,8 +4,24 @@ import epi.test_framework.GenericTest;
 public class SwapBits {
   @EpiTest(testDataFile = "swap_bits.tsv")
   public static long swapBits(long x, int i, int j) {
-    // TODO - you fill in here.
-    return 0;
+    if (i < 0 || j < 0 || i > 63 || j > 63) throw new IllegalArgumentException();
+
+    long imask = 1L << i; tobin(imask);
+    long jmask = 1L << j; tobin(jmask);
+
+    long ival = x & imask; // >0: 1, =0: 0
+    long jval = x & jmask;
+
+    if (jval > 0) x |= imask;
+    else          x &= ~imask;
+    if (ival > 0) x |= jmask;
+    else          x &= ~jmask;
+
+    return x;
+  }
+
+  private static void tobin(long x) {
+    System.out.println(Long.toBinaryString(x));
   }
 
   public static void main(String[] args) {
