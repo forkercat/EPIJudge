4e106014a3f04ac647d97f96798b716c600ff72a
Junhao Wang <junhaowww@outlook.com>
2019-06-15 18:04:29 -0700

    4.1 Parity

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -4,8 +4,13 @@ import epi.test_framework.GenericTest;
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    x ^= x >>> 32;
+    x ^= x >>> 16;
+    x ^= x >>> 8;
+    x ^= x >>> 4;
+    x ^= x >>> 2;
+    x ^= x >>> 1;
+    return (short) (x & 0x1);  // or (x & 1)
   }
 
   public static void main(String[] args) {
@@ -15,4 +20,28 @@ public class Parity {
                                 new Object() {}.getClass().getEnclosingClass())
             .ordinal());
   }
+
+  /*
+  // brute-force
+  public static short parity(long x) {
+    short result = 0;
+    while (x != 0) {
+      result ^= (x & 1); // accumulative count: 0 <-> 1
+      x >>>= 1;
+    }
+    return result;
+  }
+   */
+
+  /*
+  // drop rightmost "1"
+  public static short parity(long x) {
+    short result = 0;
+    while (x != 0) {
+      result ^= 1;   // count
+      x &= (x - 1);  // remove rightmost "1" bit
+    }
+    return result;
+  }
+   */
 }
