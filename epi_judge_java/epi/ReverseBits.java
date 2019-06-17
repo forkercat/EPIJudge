5fcf2e83fca4a4435fef73f16f1298100b4ed33e
Junhao Wang <junhaowww@outlook.com>
2019-06-17 01:19:20 -0700

    4.3 ReverseBits

--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -4,8 +4,15 @@ import epi.test_framework.GenericTest;
 public class ReverseBits {
   @EpiTest(testDataFile = "reverse_bits.tsv")
   public static long reverseBits(long x) {
-    // TODO - you fill in here.
-    return 0;
+    // In book (lookup table)
+    return x;
+  }
+
+  public static long swapBits(long x, int i, int j) {
+    if (((x >>> i) & 1) != ((x >>> j) & 1)) { // diff
+      x ^= (1L << i) | (1L << j);
+    }
+    return x;
   }
 
   public static void main(String[] args) {
@@ -15,4 +22,14 @@ public class ReverseBits {
                                 new Object() {}.getClass().getEnclosingClass())
             .ordinal());
   }
+  /*
+  // brute-force
+  public static long reverseBits(long x) {
+    for (int i = 0; i < 32; ++i) {
+      int j = 64 - i - 1;
+      x = swapBits(x, i, j);
+    }
+    return x;
+  }
+   */
 }
