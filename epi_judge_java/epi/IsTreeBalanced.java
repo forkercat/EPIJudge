10052284a20e2dfa17e8b5455e9a3e3a3a76bc21
Junhao Wang <junhaowww@outlook.com>
2019-06-24 10:10:31 -0700

    9.10 - TreeWithParentInorder

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -1,16 +1,93 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsTreeBalanced {
 
-  @EpiTest(testDataFile = "is_tree_balanced.tsv")
+  private static Map<BinaryTreeNode<Integer>, Integer> heightMap = new HashMap<>();
 
+  @EpiTest(testDataFile = "is_tree_balanced.tsv")
   public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    return isBalancedRec(tree);
+    // return isBalancedSolution(tree);
   }
 
+
+  /**
+   * Brute-Force
+   */
+  public static boolean isBalancedRec(BinaryTreeNode<Integer> tree) {
+    if (tree == null) {
+      return true;
+    }
+    if (!isBalancedRec(tree.left)) {
+      return false;
+    }
+    if (!isBalancedRec(tree.right)) {
+      return false;
+    }
+    int diff = Math.abs(height(tree.left) - height(tree.right));
+    return (diff <= 1);
+  }
+
+  // okay
+  private static int height(BinaryTreeNode<Integer> x) {
+    if (x == null) return -1;  // should be -1
+    if (heightMap.containsKey(x)) {
+      return heightMap.get(x);
+    }
+
+    int ht = Math.max(height(x.left), height(x.right)) + 1;
+    heightMap.put(x, ht);
+
+    return ht;
+  }
+
+  /**
+   * Solution
+   */
+  private static class BalanceStatusWithHeight {
+    public boolean balanced;
+    public int height;
+    public BalanceStatusWithHeight(boolean balanced, int height) {
+      this.balanced = balanced;
+      this.height = height;
+    }
+  }
+
+  public static boolean isBalancedSolution(BinaryTreeNode<Integer> tree) {
+    return checkBalanced(tree).balanced;
+  }
+
+  private static BalanceStatusWithHeight checkBalanced(BinaryTreeNode<Integer> tree) {
+    if (tree == null) {
+      return new BalanceStatusWithHeight(true, -1); // Base case
+    }
+    BalanceStatusWithHeight leftResult = checkBalanced(tree.left);
+    if (!leftResult.balanced) {
+      return leftResult; // left tree is not balanced
+    }
+    BalanceStatusWithHeight rightResult = checkBalanced(tree.right);
+    if (!rightResult.balanced) {
+      return rightResult; // right tree is not balanced
+    }
+
+    boolean isBalanced = Math.abs(leftResult.height - rightResult.height) <= 1;
+    int height = Math.max(leftResult.height, rightResult.height) + 1;
+    return new BalanceStatusWithHeight(isBalanced, height);
+  }
+
+
+
+
+
   public static void main(String[] args) {
+
+
     System.exit(
         GenericTest
             .runFromAnnotations(args, "IsTreeBalanced.java",
--- epi_judge_java/epi/TreeWithParentInorder.java
+++ epi_judge_java/epi/TreeWithParentInorder.java
@@ -1,14 +1,42 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
 import java.util.List;
 public class TreeWithParentInorder {
   @EpiTest(testDataFile = "tree_with_parent_inorder.tsv")
 
+
+  // Traverse left, visit, traverse right
   public static List<Integer> inorderTraversal(BinaryTree<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+    BinaryTree<Integer> prev = null, p = tree;
+    List<Integer> result = new ArrayList<>();
+
+    while (p != null) {
+      BinaryTree<Integer> next;
+      // The outer if controls the direction
+      if (p.parent == prev) { // we came down from prev to p
+        if (p.left != null) { // keep going left
+          next = p.left;
+        } else { // done with left
+          result.add(p.data);
+          next = (p.right != null) ? p.right : p.parent; // or prev
+        }
+      } else if (p.left == prev) { // done with left
+        result.add(p.data);
+        next = (p.right != null) ? p.right : p.parent;
+      } else { // done with both children
+        next = p.parent;
+      }
+      prev = p;
+      p = next;
+    }
+    return result;
   }
+
+
 
   public static void main(String[] args) {
     System.exit(
