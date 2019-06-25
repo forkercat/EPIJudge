590c872ff38a1aad416daccac71844b7076f8960
Junhao Wang <junhaowww@outlook.com>
2019-06-24 19:42:48 -0700

    9.10 - TreeWithParentInorder / Preorder / Postorder

--- epi_judge_java/epi/IsTreeABst.java
+++ epi_judge_java/epi/IsTreeABst.java
@@ -1,12 +1,23 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsTreeABst {
   @EpiTest(testDataFile = "is_tree_a_bst.tsv")
 
   public static boolean isBinaryTreeBST(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    return isBSTHelper(tree, Long.MIN_VALUE, Long.MAX_VALUE);
+  }
+
+  private static boolean isBSTHelper(BinaryTreeNode<Integer> x, long lower, long upper) {
+    if (x == null) {
+      return true;
+    } else if (x.data < lower || x.data > upper) {
+      return false;
+    } else {
+      return isBSTHelper(x.left, lower, x.data) && isBSTHelper(x.right, x.data, upper);
+    }
   }
 
   public static void main(String[] args) {
--- epi_judge_java/epi/TreeInorder.java
+++ epi_judge_java/epi/TreeInorder.java
@@ -1,13 +1,33 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+
 public class TreeInorder {
   @EpiTest(testDataFile = "tree_inorder.tsv")
 
   public static List<Integer> inorderTraversal(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> result = new ArrayList<>();
+    Stack<BinaryTreeNode<Integer>> stack = new Stack<>();
+    BinaryTreeNode<Integer> p = tree;
+
+    while (p != null || !stack.isEmpty()) {
+      // go to the left most
+      while (p != null) {
+        stack.push(p);
+        p = p.left;
+      } // now: p -> null
+      // visit
+      p = stack.pop();
+      result.add(p.data);
+      // go right
+      p = p.right;
+    }
+    return result;
   }
 
   public static void main(String[] args) {
