b631fe1fbffd6cf5935dba1f0d33b803360ca462
Junhao Wang <junhaowww@outlook.com>
2019-06-26 10:17:19 -0700

    14.2 - SearchFirstGreaterValueInBst.java

--- epi_judge_java/epi/SearchFirstGreaterValueInBst.java
+++ epi_judge_java/epi/SearchFirstGreaterValueInBst.java
@@ -3,11 +3,42 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class SearchFirstGreaterValueInBst {
 
+  // public static BstNode<Integer> findFirstGreaterThanK(BstNode<Integer> tree,
+  //                                                      Integer k) {
+  //   if (tree == null) {
+  //     return null;
+  //   }
+  //   BstNode<Integer> p = tree;
+  //   BstNode<Integer> succ = null;
+  //   while (p != null) {
+  //     if (k < p.data) { // go left
+  //       succ = p;
+  //       p = p.left;
+  //     } else if (k > p.data) { // go right
+  //       p = p.right;
+  //     } else { // equal
+  //       p = p.right;
+  //     }
+  //   }
+  //   return succ;
+  // }
+
   public static BstNode<Integer> findFirstGreaterThanK(BstNode<Integer> tree,
                                                        Integer k) {
-    // TODO - you fill in here.
-    return null;
+    if (tree == null) {
+      return null;
+    }
+
+    if (k < tree.data) { // go left
+      BstNode<Integer> succ = findFirstGreaterThanK(tree.left, k);
+      if (succ != null) return succ;
+      else return tree;
+    } else {
+      return findFirstGreaterThanK(tree.right, k);
+    }
   }
+
+
   @EpiTest(testDataFile = "search_first_greater_value_in_bst.tsv")
   public static int findFirstGreaterThanKWrapper(BstNode<Integer> tree,
                                                  Integer k) {
--- epi_judge_java/epi/SuccessorInTree.java
+++ epi_judge_java/epi/SuccessorInTree.java
@@ -5,10 +5,44 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
 public class SuccessorInTree {
 
+  // No right child cases:
+  // [O] indicates a successor
+  //
+  //    [O]   O
+  //    /      \
+  //   O        O    <--- p
+  //
+  //     O        [O]      O    O
+  //      \       /       /      \
+  //      [O]    O      [O]       O
+  //      /       \     /          \
+  //     O         O   O            O    <--- p
+  //
+  // If "/" exists, there is a successor.
+
   public static BinaryTree<Integer> findSuccessor(BinaryTree<Integer> node) {
-    // TODO - you fill in here.
+    BinaryTree<Integer> p = node;
+
+    // has right child
+    if (p.right != null) { // find the leftmost node
+      p = p.right;
+      while (p.left != null) p = p.left;
+      return p;
+    }
+
+    // has no right child
+    while (p.parent != null) {
+      if (p.parent.left == p) { // parent's left child == p
+        return p.parent;
+      }
+      p = p.parent;
+    }
     return null;
   }
+
+
+
+
   @EpiTest(testDataFile = "successor_in_tree.tsv")
   public static int findSuccessorWrapper(TimedExecutor executor,
                                          BinaryTree<Integer> tree, int nodeIdx)
