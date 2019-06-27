f32dfe2b8ba2c831b86e1a23e57ef1443e3d7ff5
Junhao Wang <junhaowww@outlook.com>
2019-06-27 16:42:09 -0700

    9.5 - Sum the root-to-leaf paths in a binary tree

--- epi_judge_java/epi/KLargestValuesInBst.java
+++ epi_judge_java/epi/KLargestValuesInBst.java
@@ -1,17 +1,92 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 import java.util.function.BiPredicate;
 public class KLargestValuesInBst {
   @EpiTest(testDataFile = "k_largest_values_in_bst.tsv")
 
+  // public static List<Integer> findKLargestInBst(BstNode<Integer> tree, int k) {
+  //   List<Integer> result = new ArrayList<>();
+  //   if (tree == null || k <= 0) {
+  //     return result;
+  //   }
+  //   helper(tree, k, result);
+  //   return result;
+  // }
+
+  // private static void helper(BstNode<Integer> x, int k, List<Integer> result) {
+  //   if (x == null) return;
+  //   // k nodes are added to result list
+  //   if (result.size() == k) return;
+  //
+  //   // go to right
+  //   if (x.right == null) { // locate at the rightmost node
+  //     result.add(x.data);
+  //   } else { // go to right
+  //     helper(x.right, k, result);
+  //     // current node
+  //     if (result.size() == k) return;
+  //     result.add(x.data);
+  //   }
+  //
+  //   // go to left
+  //   if (x.left != null) {
+  //     helper(x.left, k, result);
+  //   }
+  // }
+
+  // private static void helper(BstNode<Integer> x, int k, List<Integer> result) {
+  //   if (x == null || result.size() == k) {
+  //     return;
+  //   }
+  //
+  //   helper(x.right, k, result);
+  //   if (result.size() < k) {
+  //     result.add(x.data);
+  //     helper(x.left, k, result);
+  //   }
+  // }
+
+
+  /**
+   * Iteration - Reverse Inorder Traversal
+   */
   public static List<Integer> findKLargestInBst(BstNode<Integer> tree, int k) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> result = new ArrayList<>();
+    if (tree == null || k <= 0) {
+      return result;
+    }
+
+    Stack<BstNode<Integer>> stack = new Stack<>();
+    BstNode<Integer> p = tree;
+    while (p != null || !stack.isEmpty()) {
+      // go to rightmost node
+      while (p != null) {
+        stack.push(p);
+        p = p.right;
+      } // now p -> null
+      p = stack.pop(); // rightmost node
+
+
+      if (result.size() < k) {
+        result.add(p.data); // add
+        if (result.size() == k) break;
+      }
+
+      p = p.left;
+    }
+    return result;
   }
+
+
+
   @EpiTestComparator
   public static BiPredicate<List<Integer>, List<Integer>> comp =
       (expected, result) -> {
--- epi_judge_java/epi/SumRootToLeaf.java
+++ epi_judge_java/epi/SumRootToLeaf.java
@@ -1,15 +1,111 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class SumRootToLeaf {
-  @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SumRootToLeaf {
+  // @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
+
+  // public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
+  //   if (tree == null) return 0;
+  //   return sum(tree, 0);
+  // }
+  //
+  // private static int sum(BinaryTreeNode<Integer> node, int result) {
+  //   if (node == null) return 0;
+  //
+  //   result = (result << 1) + node.data;
+  //
+  //   if (node.left == null && node.right == null) {
+  //     return result;
+  //   }
+  //
+  //   int x = sum(node.left, result);
+  //   int y = sum(node.right, result);
+  //
+  //   return x + y;
+  // }
+
+
+  /** Brute-force */
+  private static HashMap<BinaryTreeNode<Integer>, BinaryTreeNode<Integer>> map;
+  private static ArrayList<BinaryTreeNode<Integer>> leaves;
+
+  @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
   public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return 0;
+    map = new HashMap<>();
+    leaves = new ArrayList<>();
+    findParentsAndLeaves(tree, null); // root's parent is null
+
+    int sum = 0; // sum up
+    for (BinaryTreeNode<Integer> leaf : leaves) {
+      int val = 0;
+      BinaryTreeNode<Integer> p = leaf;
+      int k;
+      for (k = 0, p = leaf; p != null; ++k, p = map.get(p)) {
+        val = val + (p.data << k);
+      }
+      sum += val;
+    }
+    return sum;
   }
 
+  // Preorder traversal
+  private static void findParentsAndLeaves(BinaryTreeNode<Integer> tree,  BinaryTreeNode<Integer> parent) {
+    if (tree == null) return;
+    // store leaves
+    if (tree.left == null && tree.right == null) leaves.add(tree);
+    // map child-parent
+    map.put(tree, parent);
+
+    findParentsAndLeaves(tree.left, tree);
+    findParentsAndLeaves(tree.right, tree);
+  }
+
+
+
+
+
   public static void main(String[] args) {
+
+    // BinaryTreeNode<Integer> b1 = new BinaryTreeNode<>(1);
+    // BinaryTreeNode<Integer> b2 = new BinaryTreeNode<>(0);
+    // BinaryTreeNode<Integer> b3 = new BinaryTreeNode<>(1);
+    // BinaryTreeNode<Integer> b4 = new BinaryTreeNode<>(1);
+    // BinaryTreeNode<Integer> b5 = new BinaryTreeNode<>(1);
+    // b1.left = b2; b1.right = b3;
+    // b2.left = b4; b2.right = b5;
+    // System.out.println(b1.hashCode()); // 1802598046
+    // System.out.println(b2.hashCode()); // 659748578
+    // System.out.println(b3.hashCode()); // 240650537
+    // System.out.println(b4.hashCode()); // 483422889
+    // System.out.println(b5.hashCode()); // 2088051243
+    /*
+    // BinaryTreeNode<Integer> b1 = new BinaryTreeNode<>(1);
+    // BinaryTreeNode<Integer> b2 = new BinaryTreeNode<>(2);
+    // BinaryTreeNode<Integer> b3 = new BinaryTreeNode<>(3);
+    // BinaryTreeNode<Integer> b4 = new BinaryTreeNode<>(4);
+    // BinaryTreeNode<Integer> b5 = new BinaryTreeNode<>(5);
+    b1.left = b2; b1.right = b3;
+    b2.left = b4; b2.right = b5;
+    System.out.println(b1.hashCode());
+    System.out.println(b2.hashCode());
+    System.out.println(b3.hashCode());
+    System.out.println(b4.hashCode());
+    System.out.println(b5.hashCode());
+
+
+    System.out.println("Sum = " + sumRootToLeaf(b1));
+
+    // findParentsAndLeaves(b1, null);
+    System.out.println(map.get(b4) == b2);
+
+    // System.out.println(leaves.get(3) == b3);
+
+    */
     System.exit(
         GenericTest
             .runFromAnnotations(args, "SumRootToLeaf.java",
