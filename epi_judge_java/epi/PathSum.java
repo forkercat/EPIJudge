a7cd90e4460bdcfd3859a5f4447aa580db6d57c7
Junhao Wang <junhaowww@outlook.com>
2019-06-28 10:27:12 -0700

    9.6 - PathSum

--- epi_judge_java/epi/PathSum.java
+++ epi_judge_java/epi/PathSum.java
@@ -6,8 +6,13 @@ public class PathSum {
 
   public static boolean hasPathSum(BinaryTreeNode<Integer> tree,
                                    int remainingWeight) {
-    // TODO - you fill in here.
-    return true;
+    if (tree == null) return false;
+
+    if (tree.left == null && tree.right == null) { // leaf
+      return remainingWeight == tree.data;
+    }
+
+    return hasPathSum(tree.left, remainingWeight - tree.data) || hasPathSum(tree.right, remainingWeight - tree.data);
   }
 
   public static void main(String[] args) {
