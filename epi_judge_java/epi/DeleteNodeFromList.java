dee08dd62d4ccb06c5c91cffe70dc1387dc2c3f0
Junhao Wang <junhaowww@outlook.com>
2019-06-29 10:31:35 -0700

    7.6 - Delete a node from a singly linked list

--- epi_judge_java/epi/DeleteNodeFromList.java
+++ epi_judge_java/epi/DeleteNodeFromList.java
@@ -1,14 +1,39 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
+
 public class DeleteNodeFromList {
 
   // Assumes nodeToDelete is not tail.
   public static void deletionFromList(ListNode<Integer> nodeToDelete) {
-    // TODO - you fill in here.
-    return;
+    if (nodeToDelete == null) return;
+
+    ListNode<Integer> p = nodeToDelete;
+    if (p.next != null) { // its next is not null (actually it is guaranteed in the book)
+      p.data = p.next.data;
+      p.next = p.next.next; // delete its successor
+    }
   }
+
+  /** Wrong code, nodeToDelete is not the head */
+  // public static void deletionFromList(ListNode<Integer> nodeToDelete) {
+  //   if (nodeToDelete == null) {
+  //     return;
+  //   }
+  //   ListNode<Integer> curr = nodeToDelete;
+  //   while (curr.next != null) { // guarantee curr.next @NotNull
+  //     if (curr.next.data == nodeToDelete.data) {
+  //       // curr    curr.next    curr.next.next
+  //       curr.next = curr.next.next; // delete
+  //       return;
+  //     }
+  //     curr = curr.next;
+  //   } // stops at the last node
+  // }
+
+
   @EpiTest(testDataFile = "delete_node_from_list.tsv")
   public static ListNode<Integer> deleteListWrapper(TimedExecutor executor,
                                                     ListNode<Integer> head,
