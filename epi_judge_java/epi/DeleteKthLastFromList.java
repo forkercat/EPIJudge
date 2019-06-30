31a0a23fcdfd3e1f124049798074493124469cea
Junhao Wang <junhaowww@outlook.com>
2019-06-29 21:21:19 -0700

    7.7 - DeleteKthLastFromList.java

--- epi_judge_java/epi/DeleteKthLastFromList.java
+++ epi_judge_java/epi/DeleteKthLastFromList.java
@@ -5,9 +5,38 @@ public class DeleteKthLastFromList {
   @EpiTest(testDataFile = "delete_kth_last_from_list.tsv")
 
   // Assumes L has at least k nodes, deletes the k-th last node in L.
+  // public static ListNode<Integer> removeKthLast(ListNode<Integer> L, int k) {
+  //   // 1 ≤ k ≤ length
+  //   ListNode<Integer> first = L;
+  //   ListNode<Integer> second = L;
+  //   for (int i = 0; i < k; ++i) second = second.next;
+  //
+  //   if (second == null) return L.next; // k = length
+  //
+  //   while (second.next != null) {
+  //     first = first.next;
+  //     second = second.next;
+  //   } // stops at last (k + 1)-th
+  //
+  //   first.next = first.next.next; // consider k = 1
+  //
+  //   return L;
+  // }
+
   public static ListNode<Integer> removeKthLast(ListNode<Integer> L, int k) {
-    // TODO - you fill in here.
-    return null;
+    ListNode<Integer> second = L;
+    for (int i = 0; i < k; ++i) second = second.next;
+
+    ListNode<Integer> dummy = new ListNode<>(-1, L);
+    ListNode<Integer> first = dummy;
+
+    while (second != null) { // including k = length case
+      first = first.next;
+      second = second.next;
+    }
+
+    first.next = first.next.next;
+    return dummy.next;
   }
 
   public static void main(String[] args) {
