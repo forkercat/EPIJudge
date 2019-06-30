94b0546c5203ed0cdc0d76119966ddf5432cad58
Junhao Wang <junhaowww@outlook.com>
2019-06-30 10:30:52 -0700

    7.8 - RemoveDuplicatesFromSortedList.java

--- epi_judge_java/epi/RemoveDuplicatesFromSortedList.java
+++ epi_judge_java/epi/RemoveDuplicatesFromSortedList.java
@@ -5,11 +5,60 @@ public class RemoveDuplicatesFromSortedList {
   @EpiTest(testDataFile = "remove_duplicates_from_sorted_list.tsv")
 
   public static ListNode<Integer> removeDuplicates(ListNode<Integer> L) {
-    // TODO - you fill in here.
-    return null;
+    if (L == null) {
+      return null;
+    }
+    ListNode<Integer> curr = L;
+    while (curr.next != null) {
+      if (curr.data == curr.next.data) { // found duplicates
+        curr.next = curr.next.next;
+      } else {
+        curr = curr.next;
+      }
+    }
+    return L;
   }
 
-  public static void main(String[] args) {
+
+  // Recursion #1
+  public static ListNode<Integer> removeDuplicates(ListNode<Integer> L) {
+    if (L == null || L.next == null) {
+      return L;
+    }
+
+    if (L.data == L.next.data) {
+      L.next = L.next.next;
+      removeDuplicates(L);
+    } else {
+      L.next = removeDuplicates(L.next);
+    }
+    return L;
+  }
+
+  // Recursion #2
+  public static ListNode<Integer> removeDuplicates(ListNode<Integer> L) {
+    return removeDuplicates(L, null);
+  }
+
+  //  1   1   1   2
+  //  p   L
+  private static ListNode<Integer> removeDuplicates(ListNode<Integer> L, Integer prev) {
+    if (L == null) {
+      return null;
+    }
+
+    if (prev != null && prev == L.data) {
+      L = L.next;
+      L = removeDuplicates(L, prev);
+    } else {
+      L.next = removeDuplicates(L.next, L.data);
+    }
+
+    return L;
+  }
+
+
+    public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "RemoveDuplicatesFromSortedList.java",
