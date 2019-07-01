1d29886c1e7da8b14126103813355475180d96f6
Junhao Wang <junhaowww@outlook.com>
2019-06-30 18:36:21 -0700

    4.10 - Update UniformRandomNumber.java

--- epi_judge_java/epi/UniformRandomNumber.java
+++ epi_judge_java/epi/UniformRandomNumber.java
@@ -14,7 +14,7 @@ public class UniformRandomNumber {
   }
 
   public static int uniformRandom(int lowerBound, int upperBound) {
-    int n = upperBound - lowerBound + 1;
+    int n = upperBound - lowerBound + 1; // number of outcomes
     int result;
     do {
       result = 0;
@@ -26,6 +26,26 @@ public class UniformRandomNumber {
     } while (result >= n);
     return lowerBound + result;
   }
+
+  // Solution
+  public static int uniformRandom(int lowerBound, int upperBound) {
+    int numberOfOutcomes = upperBound - lowerBound + 1;
+    int result;
+
+    do {
+      result = 0;
+      // 100    < number of outcomes
+      // i = 2
+      // [] [] [] []
+      // 16 8  4  2  represents #outcomes, if 16 is not enough, go on adding to the result
+      for (int i = 0; (1 << i) < numberOfOutcomes; ++i) {
+        result = (result << 1) | zeroOneRandom(); // place the 0/1 digit
+      }
+    } while (result >= numberOfOutcomes); // since the result is in [0 ~ #outcome - 1]
+
+    return result + lowerBound;
+  }
+
 
   private static double log2(int x) {
     return Math.log(x) / Math.log(2);
