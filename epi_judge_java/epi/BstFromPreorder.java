514b389387201009c96bf7a47f9289229136e105
Junhao Wang <junhaowww@outlook.com>
2019-07-02 15:54:33 -0700

    14.5 - BstFromPreorder.java

--- epi_judge_java/epi/BstFromPreorder.java
+++ epi_judge_java/epi/BstFromPreorder.java
@@ -5,10 +5,34 @@ import java.util.List;
 public class BstFromPreorder {
   @EpiTest(testDataFile = "bst_from_preorder.tsv")
 
-  public static BstNode<Integer>
-  rebuildBSTFromPreorder(List<Integer> preorderSequence) {
-    // TODO - you fill in here.
-    return null;
+  public static BstNode<Integer> rebuildBSTFromPreorder(List<Integer> preorderSequence) {
+    if (preorderSequence == null) {
+      return null;
+    }
+    return construct(preorderSequence, 0, preorderSequence.size() - 1);
+  }
+
+  private static BstNode<Integer> construct(List<Integer> preorder, int lo, int hi) {
+    if (preorder == null) {
+      return null;
+    }
+
+    if (lo > hi) {
+      return null;
+    }
+
+    BstNode<Integer> root = new BstNode<>(preorder.get(lo));
+    int rightIdx = lo + 1;
+
+    while (rightIdx <= hi && preorder.get(rightIdx) <= root.data) {
+      rightIdx += 1;
+    }
+
+    // left
+    root.left = construct(preorder, lo + 1, rightIdx - 1);
+    root.right = construct(preorder, rightIdx, hi);
+
+    return root;
   }
 
   public static void main(String[] args) {
