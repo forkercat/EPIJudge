e90cc10329be1582188dda598f796ed15b9e864a
Junhao Wang <junhaowww@outlook.com>
2019-07-02 16:21:55 -0700

    14.5 - BstFromPreorder.java (Two Ways)

--- epi_judge_java/epi/BstFromPreorder.java
+++ epi_judge_java/epi/BstFromPreorder.java
@@ -1,41 +1,77 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 import java.util.List;
 public class BstFromPreorder {
-  @EpiTest(testDataFile = "bst_from_preorder.tsv")
 
+  // @EpiTest(testDataFile = "bst_from_preorder.tsv")
+  // public static BstNode<Integer> rebuildBSTFromPreorder(List<Integer> preorderSequence) {
+  //   if (preorderSequence == null) {
+  //     return null;
+  //   }
+  //   return construct(preorderSequence, 0, preorderSequence.size() - 1);
+  // }
+  //
+  // private static BstNode<Integer> construct(List<Integer> preorder, int lo, int hi) {
+  //   if (preorder == null) {
+  //     return null;
+  //   }
+  //
+  //   if (lo > hi) {
+  //     return null;
+  //   }
+  //
+  //   BstNode<Integer> root = new BstNode<>(preorder.get(lo));
+  //   int rightIdx = lo + 1;
+  //
+  //   while (rightIdx <= hi && preorder.get(rightIdx) <= root.data) {
+  //     rightIdx += 1;
+  //   }
+  //
+  //   // left
+  //   root.left = construct(preorder, lo + 1, rightIdx - 1);
+  //   root.right = construct(preorder, rightIdx, hi);
+  //
+  //   return root;
+  // }
+
+
+  private static Integer rootIdx;
+
+  @EpiTest(testDataFile = "bst_from_preorder.tsv")
   public static BstNode<Integer> rebuildBSTFromPreorder(List<Integer> preorderSequence) {
     if (preorderSequence == null) {
       return null;
     }
-    return construct(preorderSequence, 0, preorderSequence.size() - 1);
+    rootIdx = 0;
+    return construct(preorderSequence, Integer.MIN_VALUE, Integer.MAX_VALUE);
   }
 
-  private static BstNode<Integer> construct(List<Integer> preorder, int lo, int hi) {
-    if (preorder == null) {
+  private static BstNode<Integer> construct(List<Integer> preorder, int lower, int upper) {
+    if (rootIdx >= preorder.size()) {
       return null;
     }
 
-    if (lo > hi) {
+    int rootVal = preorder.get(rootIdx);
+
+    if (rootVal < lower || rootVal > upper) { // rootVal >= upper
       return null;
     }
 
-    BstNode<Integer> root = new BstNode<>(preorder.get(lo));
-    int rightIdx = lo + 1;
+    rootIdx += 1;
 
-    while (rightIdx <= hi && preorder.get(rightIdx) <= root.data) {
-      rightIdx += 1;
-    }
-
-    // left
-    root.left = construct(preorder, lo + 1, rightIdx - 1);
-    root.right = construct(preorder, rightIdx, hi);
+    BstNode<Integer> root = new BstNode<>(rootVal);
+    root.left = construct(preorder, lower, rootVal);
+    root.right = construct(preorder, rootVal, upper);
 
     return root;
   }
 
-  public static void main(String[] args) {
+
+
+    public static void main(String[] args) {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "BstFromPreorder.java",
