8ef59fa035dc491fe992e9705d8513a63403875c
Junhao Wang <junhaowww@outlook.com>
2019-07-03 10:38:54 -0700

    7.9 - ListCyclicRightShift.java

--- epi_judge_java/epi/ListCyclicRightShift.java
+++ epi_judge_java/epi/ListCyclicRightShift.java
@@ -4,10 +4,34 @@ import epi.test_framework.GenericTest;
 public class ListCyclicRightShift {
   @EpiTest(testDataFile = "list_cyclic_right_shift.tsv")
 
-  public static ListNode<Integer> cyclicallyRightShiftList(ListNode<Integer> L,
-                                                           int k) {
-    // TODO - you fill in here.
-    return null;
+  public static ListNode<Integer> cyclicallyRightShiftList(ListNode<Integer> L, int k) {
+    if (L == null) {
+      return null;
+    }
+
+    int len = 1;
+    ListNode<Integer> p = L;
+    while (p.next != null) {
+      p = p.next;
+      ++len;
+    }
+    p.next = L; // make a ring
+
+    //  1  2  3  4  5, k = 2
+    //        p
+    //  4  5  1  2  3
+    k = k % len;
+    if (k == 0) return L;
+    int step = len - k - 1;
+    p = L;
+    for (int i = 0; i < step; ++i) {
+      p = p.next;
+    }
+
+    ListNode<Integer> newHead = p.next;
+    p.next = null; // remove the ring
+
+    return newHead;
   }
 
   public static void main(String[] args) {
