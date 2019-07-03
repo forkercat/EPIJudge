22e2705097ea8d4503cbd75579270f62084e8cd8
Junhao Wang <junhaowww@outlook.com>
2019-07-03 10:46:08 -0700

    7.13 - IntAsListAdd.java

--- epi_judge_java/epi/IntAsListAdd.java
+++ epi_judge_java/epi/IntAsListAdd.java
@@ -4,10 +4,26 @@ import epi.test_framework.GenericTest;
 public class IntAsListAdd {
   @EpiTest(testDataFile = "int_as_list_add.tsv")
 
-  public static ListNode<Integer> addTwoNumbers(ListNode<Integer> L1,
-                                                ListNode<Integer> L2) {
-    // TODO - you fill in here.
-    return null;
+  public static ListNode<Integer> addTwoNumbers(ListNode<Integer> L1, ListNode<Integer> L2) {
+    return add(L1, L2, 0);
+  }
+
+  private static ListNode<Integer> add(ListNode<Integer> L1, ListNode<Integer> L2, int carry) {
+    if (L1 == null && L2 == null) {
+      if (carry > 0) {
+        return new ListNode<>(carry, null);
+      } else {
+        return null;
+      }
+    }
+    int x = (L1 == null) ? 0 : L1.data;
+    int y = (L2 == null) ? 0 : L2.data;
+    int sum = x + y + carry;
+    ListNode retNode = new ListNode(sum % 10, null);
+    if (L1 != null) L1 = L1.next;
+    if (L2 != null) L2 = L2.next;
+    retNode.next = add(L1, L2, sum / 10);
+    return retNode;
   }
 
   public static void main(String[] args) {
