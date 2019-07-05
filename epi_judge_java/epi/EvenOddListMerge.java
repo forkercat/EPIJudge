897fc5f8fe3bd8858a182b5c128ced56b6da0ff6
Junhao Wang <junhaowww@outlook.com>
2019-07-05 11:18:07 -0700

    7.10 - EvenOddListMerge.java

--- epi_judge_java/epi/EvenOddListMerge.java
+++ epi_judge_java/epi/EvenOddListMerge.java
@@ -1,13 +1,31 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
 public class EvenOddListMerge {
   @EpiTest(testDataFile = "even_odd_list_merge.tsv")
 
   public static ListNode<Integer> evenOddMerge(ListNode<Integer> L) {
-    // TODO - you fill in here.
-    return null;
+    ListNode<Integer> evenDummy = new ListNode<>(-1, null);
+    ListNode<Integer> oddDummy = new ListNode<>(-1, null);
+    ListNode<Integer> evenPrev = evenDummy;
+    ListNode<Integer> oddPrev = oddDummy;
+
+    int count = 0;
+    while (L != null) {
+      if ((count & 1) == 0) { // even
+        evenPrev.next = L;
+        evenPrev = evenPrev.next;
+      } else { // odd
+        oddPrev.next = L;
+        oddPrev = oddPrev.next;
+      }
+      count ^= 1;
+      L = L.next;
+    }
+    // evenPrev -> the last node of even list
+    evenPrev.next = oddDummy.next;
+    oddPrev.next = null; // important. set the tail
+    return evenDummy.next;
   }
 
   public static void main(String[] args) {
