71f4d7c0abb0ede8fc70fd9525b732ed3e7de6be
Junhao Wang <junhaowww@outlook.com>
2019-07-08 10:17:31 -0700

    14.8 - BstFromSortedArray.java

--- epi_judge_java/epi/BstFromSortedArray.java
+++ epi_judge_java/epi/BstFromSortedArray.java
@@ -1,18 +1,63 @@
 package epi;
-import epi.test_framework.BinaryTreeUtils;
-import epi.test_framework.EpiTest;
-import epi.test_framework.GenericTest;
-import epi.test_framework.TestFailure;
-import epi.test_framework.TestUtils;
-import epi.test_framework.TimedExecutor;
+import epi.test_framework.*;
+
 import java.util.List;
 public class BstFromSortedArray {
 
-  public static BstNode<Integer>
-  buildMinHeightBSTFromSortedArray(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
+  // Space: O(h)
+  // public static BstNode<Integer> buildMinHeightBSTFromSortedArray(List<Integer> A) {
+  //   //   if (A == null || A.size() == 0) {
+  //   //     return null;
+  //   //   }
+  //   //   return construct(A, 0, A.size() - 1);
+  //   // }
+  //   //
+  //   //
+  //   // private static BstNode<Integer> construct(List<Integer> A, int lo, int hi) {
+  //   //   if (lo > hi) {
+  //   //     return null;
+  //   //   }
+  //   //   if (lo == hi) { // only one node
+  //   //     return new BstNode<>(A.get(lo));
+  //   //   }
+  //   //   int mid = lo + (hi - lo) / 2;
+  //   //   BstNode<Integer> root = new BstNode<>(A.get(mid));
+  //   //   root.left = construct(A, lo, mid - 1);
+  //   //   root.right = construct(A, mid + 1, hi);
+  //   //   return root;
+  //   // }
+
+  private static int headIdx;
+  public static BstNode<Integer> buildMinHeightBSTFromSortedArray(List<Integer> A) {
+    if (A == null || A.size() == 0) {
+      return null;
+    }
+    headIdx = 0;
+    return construct(A, 0, A.size() - 1);
   }
+
+  private static BstNode<Integer> construct(List<Integer> A, int lo, int hi) {
+    if (lo > hi) {
+      return null;
+    }
+    int mid = lo + (hi - lo) / 2;
+
+    BstNode<Integer> left = construct(A, lo, mid - 1);
+
+    BstNode<Integer> root = new BstNode<>(A.get(headIdx));
+
+    headIdx += 1;
+
+    BstNode<Integer> right = construct(A, mid + 1, hi);
+
+    root.left = left;
+    root.right = right;
+
+    return root;
+  }
+
+
+
   @EpiTest(testDataFile = "bst_from_sorted_array.tsv")
   public static int
   buildMinHeightBSTFromSortedArrayWrapper(TimedExecutor executor,
