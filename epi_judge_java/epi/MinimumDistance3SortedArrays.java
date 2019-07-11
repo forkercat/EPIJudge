c5a0e46bd0798eb1d68f177984573f97912afe10
Junhao Wang <junhaowww@outlook.com>
2019-07-11 11:49:36 -0700

    MinimumDistance3SortedArrays (not finished)

--- epi_judge_java/epi/MinimumDistance3SortedArrays.java
+++ epi_judge_java/epi/MinimumDistance3SortedArrays.java
@@ -25,11 +25,41 @@ public class MinimumDistance3SortedArrays {
 
   @EpiTest(testDataFile = "minimum_distance_3_sorted_arrays.tsv")
 
-  public static int
-  findMinDistanceSortedArrays(List<List<Integer>> sortedArrays) {
-    // TODO - you fill in here.
-    return 0;
+  public static int findMinDistanceSortedArrays(List<List<Integer>> sortedArrays) {
+    int k = sortedArrays.size();
+    System.out.println(k);
+    int[] index = new int[k];
+    int minResult = Integer.MAX_VALUE;
+    boolean isFinished = false;
+    while (isFinished == false) {
+      int maxIdx = -1, minIdx = -1;
+      int maxVal = Integer.MIN_VALUE, minVal = Integer.MAX_VALUE;
+      for (int i = 0; i < k; ++i) {
+        int currIndex = index[i];
+        if (currIndex >= sortedArrays.get(i).size()) {
+          isFinished = true;
+          break;
+        }
+        int val = sortedArrays.get(i).get(currIndex);
+        if (val < minVal) {
+          minVal = val;
+          minIdx = i;
+        }
+        if (val > maxVal) {
+          maxVal = val;
+          maxIdx = i;
+        }
+      }
+      if (maxIdx != -1 && minIdx != -1) {
+        minResult = Math.min(minResult, maxVal - minVal);
+        index[minIdx] += 1;
+      }
+    }
+
+    return minResult;
   }
+
+
 
   public static void main(String[] args) {
     System.exit(
