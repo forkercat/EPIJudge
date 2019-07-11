5fd237981adfe0d233f2f99ed6d0bf2572d4d9c7
Junhao Wang <junhaowww@outlook.com>
2019-07-11 11:49:20 -0700

    DescendantAndAncestorInBst.java

--- epi_judge_java/epi/DescendantAndAncestorInBst.java
+++ epi_judge_java/epi/DescendantAndAncestorInBst.java
@@ -5,13 +5,63 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
 public class DescendantAndAncestorInBst {
 
-  public static boolean
-  pairIncludesAncestorAndDescendantOfM(BstNode<Integer> possibleAncOrDesc0,
-                                       BstNode<Integer> possibleAncOrDesc1,
-                                       BstNode<Integer> middle) {
-    // TODO - you fill in here.
+
+
+  public static boolean pairIncludesAncestorAndDescendantOfM(BstNode<Integer> p1, BstNode<Integer> p2, BstNode<Integer> middle) {
+    if (p1 == null || p2 == null || middle == null) {
+      throw new IllegalArgumentException();
+    }
+    return check(p1, p2, middle) || check(p2, p1, middle);
+  }
+
+
+  private static boolean check(BstNode<Integer> ancestor, BstNode<Integer> descendant, BstNode<Integer> middle) {
+    // check ancestor
+    BstNode<Integer> p = ancestor;
+    boolean isAncestor = false;
+    while (p != null) { // from ancestor, search middle
+      if (middle.data < p.data) {
+        p = p.left;
+      } else if (middle.data > p.data) {
+        p = p.right;
+      } else {
+        isAncestor = true;
+        break;
+      }
+    }
+    System.out.println();
+    System.out.println("isAncestor = " + ancestor.data + " => " + isAncestor);
+    System.out.println("middle = " + middle.data);
+
+    if (isAncestor == false) return false;
+    else if (ancestor == middle) return false;
+
+    // check descendant
+    p = middle;
+    boolean isDescendant = false;
+    while (p != null) { // from middle, search descendant
+      if (descendant.data < p.data) {
+        p = p.left;
+      } else if (descendant.data > p.data) {
+        p = p.right;
+      } else {
+        isDescendant = true;
+        break;
+      }
+    }
+
+    if (isDescendant == false) return false;
+
+    else if (descendant == middle) return false;
+
+    // if (descendant == middle && ancestor == middle) return false;
+
     return true;
   }
+
+
+
+
   @EpiTest(testDataFile = "descendant_and_ancestor_in_bst.tsv")
   public static boolean pairIncludesAncestorAndDescendantOfMWrapper(
       TimedExecutor executor, BstNode<Integer> tree, int possibleAncOrDesc0,
