662d18f1e1ce2dc991c2d29a3e55b46bb9ca2791
Junhao Wang <junhaowww@outlook.com>
2019-07-16 19:00:15 -0700

    11.3 - SearchShiftedSortedArray.java

--- epi_judge_java/epi/SearchEntryEqualToIndex.java
+++ epi_judge_java/epi/SearchEntryEqualToIndex.java
@@ -6,10 +6,59 @@ import epi.test_framework.TimedExecutor;
 import java.util.List;
 public class SearchEntryEqualToIndex {
 
+  // Brute-Force O(N)
+  // public static int searchEntryEqualToItsIndex(List<Integer> A) {
+  //   for (int i = 0; i < A.size(); ++i) {
+  //     if (i == A.get(i)) {
+  //       return i;
+  //     }
+  //   }
+  //   return -1;
+  // }
+
+
+
   public static int searchEntryEqualToItsIndex(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
+    // define range
+    int lo = 0, hi = A.size() - 1;
+    while (lo <= hi) {
+      int mid = lo + (hi - lo) / 2;
+      if (A.get(mid) == mid) {
+        return mid;
+      }
+      if (A.get(mid) > mid) {
+        hi = mid - 1;
+      } else {
+        lo = mid + 1;
+      }
+    }
+    return -1;
   }
+
+
+  // Binary Search O(logN)
+  //
+  // public static int searchEntryEqualToItsIndex(List<Integer> A) {
+  //   // define range
+  //   int lo = 0, hi = A.size() - 1;
+  //   while (lo <= hi) {
+  //     int mid = lo + (hi - lo) / 2;
+  //     if (A.get(mid) >= mid) { // g(mid)
+  //       hi = mid - 1;
+  //     } else {
+  //       lo = mid + 1;
+  //     }
+  //   }
+  //   // check return
+  //   if (lo < A.size() && A.get(lo) == lo) {
+  //     return lo;
+  //   } else {
+  //     return -1;
+  //   }
+  // }
+
+
+
   @EpiTest(testDataFile = "search_entry_equal_to_index.tsv")
   public static void searchEntryEqualToItsIndexWrapper(TimedExecutor executor,
                                                        List<Integer> A)
--- epi_judge_java/epi/SearchShiftedSortedArray.java
+++ epi_judge_java/epi/SearchShiftedSortedArray.java
@@ -1,14 +1,70 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 import java.util.List;
+
 public class SearchShiftedSortedArray {
   @EpiTest(testDataFile = "search_shifted_sorted_array.tsv")
 
+  // brute-force
+  // public static int searchSmallest(List<Integer> A) {
+  //   for (int i = 0; i < A.size() - 1; ++i) {
+  //     if (A.get(i) > A.get(i + 1)) {
+  //       return i + 1;
+  //     }
+  //   }
+  //   return 0;
+  // }
+
+  // Note: Our goal is to find the minimum value.
+  //          [3]
+  //  0  1  2  3  4  5  6
+  //  7  9  1  2  3  4  5
+  //        ^
+  //          [3]
+  //  0  1  2  3  4  5  6
+  //  5  7  9  1  2  3  4
+  //           1 < 4         => go left
+  // [0]
+  //  0  1  2  3  4  5  6
+  //  1  2  3  4  5  7  9
+  //           4 < 9         => go left
+  //                   [6]
+  //  0  1  2  3  4  5  6
+  //  2  3  4  5  7  9  1
+  //           5 > 1         => go right
+  //
   public static int searchSmallest(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
+    int n = A.size();
+    int lo = 0, hi = n - 1;
+
+    while (lo < hi) { // variant if you want to stop at lo == hi
+      int mid = lo + (hi - lo) / 2;
+      if (A.get(mid) < A.get(n - 1)) {
+        hi = mid; // left (variant)
+      } else {
+        lo = mid + 1;
+      }
+    }
+    return lo;
   }
+
+  // based on the above offset, search the value val.
+  public static int binarySearchByOffset(List<Integer> A, int val, int offset) {
+    int lo = 0, hi = A.size() - 1;
+    while (lo <= hi) {
+      int mid = lo + (hi - lo) / 2;
+      if (A.get(mid + offset) >= val) {
+        hi = mid - 1;
+      } else {
+        lo = mid + 1;
+      }
+    }
+    return lo + offset;
+  }
+
 
   public static void main(String[] args) {
     System.exit(
