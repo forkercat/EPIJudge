0edfc8c70cce593680c235405118339db1be6322
Junhao Wang <junhaowww@outlook.com>
2019-07-17 00:46:36 -0700

    11.5 - RealSquareRoot (not passed)

--- epi_judge_java/epi/RealSquareRoot.java
+++ epi_judge_java/epi/RealSquareRoot.java
@@ -5,8 +5,24 @@ public class RealSquareRoot {
   @EpiTest(testDataFile = "real_square_root.tsv")
 
   public static double squareRoot(double x) {
-    // TODO - you fill in here.
-    return 0.0;
+    double eps = 0.000001;
+    double lo, hi;
+    if (x < 1.0) {
+      lo = x; hi = 1.0;
+    } else {
+      lo = 1.0; hi = x;
+    }
+    // Keeps searching as long as lo != hi, within tolerance
+    while (Math.abs(lo - hi) > eps) {
+      double mid = lo + 0.5 * (hi - lo);
+      double midSquared = mid * mid;
+      if (midSquared >= x) {
+        hi = mid;
+      } else {
+        lo = mid;
+      }
+    }
+    return lo;
   }
 
   public static void main(String[] args) {
