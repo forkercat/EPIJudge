968d2dca903c22809dccdab170fe412a2784f740
Junhao Wang <junhaowww@outlook.com>
2019-08-17 13:08:46 -0700

    5.1 - The Dutch National Flag Problem

--- epi_judge_java/epi/DutchNationalFlag.java
+++ epi_judge_java/epi/DutchNationalFlag.java
@@ -1,17 +1,91 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 public class DutchNationalFlag {
   public enum Color { RED, WHITE, BLUE }
 
+
+
+  // brute-force
+
   public static void dutchFlagPartition(int pivotIndex, List<Color> A) {
-    // TODO - you fill in here.
-    return;
+    Color pivot = A.get(pivotIndex);
+    // First pass: group elements smaller than pivot.
+    for (int i = 0; i < A.size(); ++i) {
+      // Look for a smaller element
+      for (int j = i + 1; j < A.size(); ++j) {
+        if (A.get(j).ordinal() < pivot.ordinal()) {
+          Collections.swap(A, i, j);
+          break;
+        }
+      }
+    }
+    // Second pass: group elements larger than pivot.
+    for (int i = A.size()  - 1; i >= 0; --i) {
+      // Stop when hitting an element smaller than the pivot.
+      if (A.get(i).ordinal() < pivot.ordinal()) break;
+
+      for (int j = i - 1; j >= 0; --j) {
+        if (A.get(j).ordinal() > pivot.ordinal()) {
+          Collections.swap(A, i, j);
+          break;
+        }
+      }
+    }
   }
+
+
+  // O(N) - Two Passes
+  /*
+  public static void dutchFlagPartition(int pivotIndex, List<Color> A) {
+    Color pivot = A.get(pivotIndex);
+    // Note: pivotIndex may change, but pivot won't change anymore.
+    // First pass
+    int count = 0;
+    for (int i = 0; i < A.size(); ++i) {
+      if (A.get(i).ordinal() < pivot.ordinal()) {
+        Collections.swap(A, i, count++);
+      }
+    }
+    // Second pass - Move elements equal to pivot
+    for (int i = count; i < A.size(); ++i) {
+      if (A.get(i).ordinal() == pivot.ordinal()) {
+        Collections.swap(A, i, count++);
+      }
+    }
+  }
+  */
+
+
+  // O(N) - One Pass / Three Pointers
+  /*
+  public static void dutchFlagPartition(int pivotIndex, List<Color> A) {
+    Color pivot = A.get(pivotIndex);
+    int smaller = 0, equal = 0, larger = A.size() - 1;
+    while (equal <= larger) {
+      // A.get(equal) is the incoming unclassified element
+      if (A.get(equal).ordinal() < pivot.ordinal()) {
+        Collections.swap(A, equal++, smaller++);
+      } else if (A.get(equal).ordinal() > pivot.ordinal()) {
+        Collections.swap(A, equal, larger--); // equal not changing since its element is not processed
+      } else { // A.get(equal).ordinal() == pivot.ordinal()
+        equal += 1;
+      }
+    }
+  }
+   */
+
+
+
+
+
   @EpiTest(testDataFile = "dutch_national_flag.tsv")
   public static void dutchFlagPartitionWrapper(TimedExecutor executor,
                                                List<Integer> A, int pivotIdx)
