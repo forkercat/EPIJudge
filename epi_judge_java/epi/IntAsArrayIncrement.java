0856d8d65a92b41bbaaf60b5a0f2b30bd808142c
Junhao Wang <junhaowww@outlook.com>
2019-08-19 10:52:34 -0700

    5.2 - IntAsArrayIncrement

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -3,11 +3,31 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import java.util.List;
 public class IntAsArrayIncrement {
+
   @EpiTest(testDataFile = "int_as_array_increment.tsv")
+
+  // Test Cases:
+  // []
+  // [1]
+  // [9, 9]
+  // [1, 2, 3]
   public static List<Integer> plusOne(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
+    int n = A.size();
+    int carry = 1;
+    for (int i = n - 1; i >= 0; --i) {
+      int curr = A.get(i);
+      A.set(i, (curr + carry) % 10);
+      carry = (curr + carry) / 10;
+    }
+    // if carry > 0, expend the array
+    if (carry > 0) {
+      A.add(0);
+      A.set(0, 1); // set the first index as 1
+    }
+    return A;
   }
+
+
 
   public static void main(String[] args) {
     System.exit(
