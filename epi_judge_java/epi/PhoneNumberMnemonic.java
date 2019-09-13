28d9819f96ac5809a9a5392f7c8255a7268fc7fe
Junhao Wang <junhaowww@outlook.com>
2019-09-13 16:30:55 -0700

    15.2 Compute All Mnemonics For A Phone Number

--- epi_judge_java/epi/PhoneNumberMnemonic.java
+++ epi_judge_java/epi/PhoneNumberMnemonic.java
@@ -1,17 +1,67 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiPredicate;
 public class PhoneNumberMnemonic {
+
+  private static final String[] MAPPING = { "0", "1", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+
   @EpiTest(testDataFile = "phone_number_mnemonic.tsv")
 
   public static List<String> phoneMnemonic(String phoneNumber) {
-    // TODO - you fill in here.
-    return null;
+    List<String> result = new ArrayList<>();
+    phoneMnemonic(0, phoneNumber, new StringBuilder(), result);
+    // phoneMnemonic(phoneNumber, new StringBuilder(), result);
+    return result;
   }
+
+  private static void phoneMnemonic(int pos, String phoneNumber, StringBuilder sb, List<String> result) {
+    // base case
+    if (pos == phoneNumber.length()) { // pos: 0 ~ len - 1
+      result.add(sb.toString());
+      return;
+    }
+
+    int num = phoneNumber.charAt(pos) - '0'; // convert from char to integer
+    String option = MAPPING[num];
+
+    for (int i = 0; i < option.length(); ++i) {
+      char ch = option.charAt(i);
+      sb.append(ch);
+      phoneMnemonic(pos + 1, phoneNumber, sb, result);
+      sb.setLength(sb.length() - 1); // remove ch
+    }
+  }
+
+  /** Actually, we don't need pos parameter */
+  private static void phoneMnemonic(String phoneNumber, StringBuilder sb, List<String> result) {
+    int pos = sb.length();
+    // base case
+    if (pos == phoneNumber.length()) { // pos: 0 ~ len - 1
+      result.add(sb.toString());
+      return;
+    }
+
+    int num = phoneNumber.charAt(pos) - '0'; // convert from char to integer
+    String option = MAPPING[num];
+
+    for (int i = 0; i < option.length(); ++i) {
+      char ch = option.charAt(i);
+      sb.append(ch);
+      phoneMnemonic(pos + 1, phoneNumber, sb, result);
+      sb.setLength(sb.length() - 1); // remove ch
+    }
+  }
+
+
+
+
   @EpiTestComparator
   public static BiPredicate<List<String>, List<String>> comp =
       (expected, result) -> {
