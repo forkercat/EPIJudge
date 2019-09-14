f3122ed4f1cc0596184a2e5b1d5c5d72328bc98c
Junhao Wang <junhaowww@outlook.com>
2019-09-13 17:22:36 -0700

    15.2 Compute All Mnemonics For A Phone Number (Variant: Solve without recursion)

--- epi_judge_java/epi/PhoneNumberMnemonic.java
+++ epi_judge_java/epi/PhoneNumberMnemonic.java
@@ -7,17 +7,20 @@ import epi.test_framework.GenericTest;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 import java.util.function.BiPredicate;
+
 public class PhoneNumberMnemonic {
 
-  private static final String[] MAPPING = { "0", "1", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+  private static final String[] MAPPING = {"0", "1", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
 
   @EpiTest(testDataFile = "phone_number_mnemonic.tsv")
 
   public static List<String> phoneMnemonic(String phoneNumber) {
     List<String> result = new ArrayList<>();
-    phoneMnemonic(0, phoneNumber, new StringBuilder(), result);
+    // phoneMnemonic(0, phoneNumber, new StringBuilder(), result);
     // phoneMnemonic(phoneNumber, new StringBuilder(), result);
+    phoneMnemonicItr(phoneNumber, result);
     return result;
   }
 
@@ -39,7 +42,9 @@ public class PhoneNumberMnemonic {
     }
   }
 
-  /** Actually, we don't need pos parameter */
+  /**
+   * Actually, we don't need pos parameter
+   */
   private static void phoneMnemonic(String phoneNumber, StringBuilder sb, List<String> result) {
     int pos = sb.length();
     // base case
@@ -60,24 +65,73 @@ public class PhoneNumberMnemonic {
   }
 
 
+  /**
+   * Without recursion
+   */
+  private static List<String> phoneMnemonicItr(String phoneNumber, List<String> result) {
+    // corner case
+    if (phoneNumber.length() == 0) {
+      return result;
+    }
+
+    // Stack & StringBuilder
+    Stack<Integer> posStack = new Stack<>();
+    Stack<String> strStack = new Stack<>();
+    StringBuilder sb = new StringBuilder();
+
+    // Init
+    String firstOption = MAPPING[phoneNumber.charAt(0) - '0'];
+    for (int i = firstOption.length() - 1; i >= 0; --i) {
+      String str = Character.toString(firstOption.charAt(i));
+      posStack.push(0); strStack.push(str);
+    }
+
+    while (posStack.size() > 0) {
+      int pos = posStack.pop();
+      String str = strStack.pop();
+
+      sb.setLength(pos); // remove extra character
+      sb.append(str);    // add current string
+
+      // base case - when pos is the last index
+      if (pos == phoneNumber.length() - 1) {
+        result.add(sb.toString());
+        continue;
+      }
+
+      // push the next strings
+      String option = MAPPING[phoneNumber.charAt(pos + 1) - '0'];
+      for (int i = option.length() - 1; i >= 0; --i) {
+        String optionStr = Character.toString(option.charAt(i));
+        posStack.push(pos + 1); strStack.push(optionStr);
+      }
+    }
+
+    return result;
+  }
+
+
+
+
 
 
   @EpiTestComparator
   public static BiPredicate<List<String>, List<String>> comp =
-      (expected, result) -> {
-    if (result == null) {
-      return false;
-    }
-    Collections.sort(expected);
-    Collections.sort(result);
-    return expected.equals(result);
-  };
+          (expected, result) -> {
+            if (result == null) {
+              return false;
+            }
+            Collections.sort(expected);
+            Collections.sort(result);
+            return expected.equals(result);
+          };
 
   public static void main(String[] args) {
     System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PhoneNumberMnemonic.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+            GenericTest
+                    .runFromAnnotations(args, "PhoneNumberMnemonic.java",
+                            new Object() {
+                            }.getClass().getEnclosingClass())
+                    .ordinal());
   }
 }
