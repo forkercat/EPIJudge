da4c2a27931f4de1d2897bf842711f7c792e10f8
Junhao Wang <junhaowww@outlook.com>
2019-09-14 09:59:30 -0700

    15.4 - Generate Permutations

--- epi_judge_java/epi/Permutations.java
+++ epi_judge_java/epi/Permutations.java
@@ -1,8 +1,11 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
-import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
+import epi.test_framework.LexicographicalListComparator;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiPredicate;
@@ -10,9 +13,34 @@ public class Permutations {
   @EpiTest(testDataFile = "permutations.tsv")
 
   public static List<List<Integer>> permutations(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
+    List<List<Integer>> result = new ArrayList<>();
+    permutation(0, A, result); // depth = 0
+    return result;
   }
+
+  private static void permutation(int depth, List<Integer> A, List<List<Integer>> result) {
+    // base case
+    if (depth == A.size() - 1) {
+      result.add(new ArrayList<>(A));
+      return;
+    }
+    // enumeration
+    for (int i = depth; i < A.size(); ++i) {
+      Collections.swap(A, depth, i);
+      permutation(depth + 1, A, result);
+      Collections.swap(A, depth, i);
+    }
+  }
+
+
+  // variant
+
+
+
+
+
+
+
   @EpiTestComparator
   public static BiPredicate<List<List<Integer>>, List<List<Integer>>> comp =
       (expected, result) -> {
