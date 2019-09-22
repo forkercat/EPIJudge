3a05f1ec5e10324bfb16ba477e8636d15cc01c60
Junhao Wang <junhaowww@outlook.com>
2019-09-21 22:55:00 -0700

    15.5 Generate The Power Set (backtracking)

--- epi_judge_java/epi/Combinations.java
+++ epi_judge_java/epi/Combinations.java
@@ -1,18 +1,59 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
-import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
-import java.util.Collections;
+import epi.test_framework.LexicographicalListComparator;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiPredicate;
 public class Combinations {
   @EpiTest(testDataFile = "combinations.tsv")
 
+
+  // 1 2 3 4 (n = 4, k = 2)
+  // 1 2
+  // 1 3
+  // 1 4
+  // 2 3
+  // 2 4
+  // 3 4
+
+  // 1 2 3 4 (n = 4, k = 3)
+  // 1= 2,3,4 (pick 2)
+  // 2= 3,4   (pick 2)
+  // 3= 4     (N/A)
+  // 4=       (N/A)
+
   public static List<List<Integer>> combinations(int n, int k) {
-    // TODO - you fill in here.
-    return null;
+    // Init
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> nums = new ArrayList<>();
+    // Backtracking
+    combinations(n, k, 1, nums, result);
+    return result;
   }
+
+  private static void combinations(int n, int k, int d, List<Integer> nums, List<List<Integer>> result) {
+    // base case
+    int numLeft = n - d + 1;
+    if (numLeft < k) {
+      return;
+    }
+
+    if (k <= 0) {
+      result.add(new ArrayList<>(nums));
+      return;
+    }
+
+    for (int i = d; i <= n; ++i) {
+      nums.add(i);
+      combinations(n, k - 1, i + 1, nums, result); // k - 1 because we have put a new number to nums
+      nums.remove(nums.size() - 1);
+    }
+  }
+
+
   @EpiTestComparator
   public static BiPredicate<List<List<Integer>>, List<List<Integer>>> comp =
       (expected, result) -> {
--- epi_judge_java/epi/PowerSet.java
+++ epi_judge_java/epi/PowerSet.java
@@ -1,18 +1,82 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
-import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
+import epi.test_framework.LexicographicalListComparator;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiPredicate;
 public class PowerSet {
   @EpiTest(testDataFile = "power_set.tsv")
 
+  /**
+   * Use the code in 15.6
+   */
+  /*
   public static List<List<Integer>> generatePowerSet(List<Integer> inputSet) {
-    // TODO - you fill in here.
-    return null;
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> nums = new ArrayList<>();
+    int n = inputSet.size();
+    for (int k = 0; k <= n; ++k) {
+      combine(n, k, 1, inputSet, nums, result);
+    }
+    return result;
   }
+
+  private static void combine(int n, int k, int d, List<Integer> inputSet, List<Integer> nums, List<List<Integer>> result) {
+    // base case
+    if (nums.size() == k) { // get a result
+      result.add(new ArrayList<>(nums));
+      return;
+    }
+    if (n - d + 1 < k - nums.size()) { // remaining elements are not enough
+      return;
+    }
+
+    for (int i = d; i <= n; ++i) {
+      int elem = inputSet.get(i - 1);
+      nums.add(elem);
+      combine(n, k, i + 1, inputSet, nums, result);
+      nums.remove(nums.size() - 1); // remove the last
+    }
+  }
+
+   */
+
+
+
+  /**
+   * Brute-force Solution
+   */
+  public static List<List<Integer>> generatePowerSet(List<Integer> inputSet) {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> numList = new ArrayList<>();
+    result.add(new ArrayList<>()); // empty set
+    subset(0, numList, inputSet, result);
+    return result;
+  }
+
+  private static void subset(int offset, List<Integer> numList, List<Integer> inputSet, List<List<Integer>> result) {
+    // base case
+    if (offset == inputSet.size()) {
+      return;
+    }
+    int val = inputSet.get(offset);
+    // pick
+    numList.add(val);
+    subset(offset + 1, numList, inputSet, result);
+    // add to result(
+    result.add(new ArrayList<>(numList));
+    // not pick
+    numList.remove(numList.size() - 1);
+    subset(offset + 1, numList, inputSet, result);
+  }
+
+
+
   @EpiTestComparator
   public static BiPredicate<List<List<Integer>>, List<List<Integer>>> comp =
       (expected, result) -> {
