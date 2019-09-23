3a5b828492f930784f4d0db37a9d360c5731f652
Junhao Wang <junhaowww@outlook.com>
2019-09-23 10:42:11 -0700

    17.3 The Interval Covering Problem

--- epi_judge_java/epi/MinimumPointsCoveringIntervals.java
+++ epi_judge_java/epi/MinimumPointsCoveringIntervals.java
@@ -1,7 +1,10 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
+
+import java.util.Collections;
 import java.util.List;
 public class MinimumPointsCoveringIntervals {
   @EpiUserType(ctorParams = {int.class, int.class})
@@ -18,8 +21,19 @@ public class MinimumPointsCoveringIntervals {
   @EpiTest(testDataFile = "minimum_points_covering_intervals.tsv")
 
   public static Integer findMinimumVisits(List<Interval> intervals) {
-    // TODO - you fill in here.
-    return 0;
+    // sort
+    Collections.sort(intervals, (o1, o2) -> (o1.right - o2.right));
+    int numVisit = 0, lastVisitTime = Integer.MIN_VALUE;
+    for (int i = 0; i < intervals.size(); ++i) {
+      // check if it is covered
+      if (intervals.get(i).left <= lastVisitTime) {
+        continue;  // cover
+      } else {
+        numVisit += 1;  // not cover
+        lastVisitTime = intervals.get(i).right;
+      }
+    }
+    return numVisit;
   }
 
   public static void main(String[] args) {
