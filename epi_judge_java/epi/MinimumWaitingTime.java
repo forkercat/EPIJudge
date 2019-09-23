9623325d546db397b8e45336230894b06780316c
Junhao Wang <junhaowww@outlook.com>
2019-09-22 18:15:09 -0700

    17.2 - Schedule to Minimize Waiting Time (Minimum Waiting Time)

--- epi_judge_java/epi/MinimumWaitingTime.java
+++ epi_judge_java/epi/MinimumWaitingTime.java
@@ -1,13 +1,26 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.Collections;
 import java.util.List;
 public class MinimumWaitingTime {
   @EpiTest(testDataFile = "minimum_waiting_time.tsv")
 
   public static int minimumTotalWaitingTime(List<Integer> serviceTimes) {
-    // TODO - you fill in here.
-    return 0;
+    // sorting
+    Collections.sort(serviceTimes);
+    int waitTime = 0, total = 0;
+    // 1    2    3    5
+    //      1    1+2  1+2+3   // each
+    //      1    4    10      // total
+    //      total: 1 + 3 + 6
+    // start with the second task, its waiting time is the length of the first task
+    for (int i = 1; i < serviceTimes.size(); ++i) {
+      waitTime += serviceTimes.get(i - 1); // previous wait time + last wait time
+      total += waitTime;
+    }
+    return total;
   }
 
   public static void main(String[] args) {
