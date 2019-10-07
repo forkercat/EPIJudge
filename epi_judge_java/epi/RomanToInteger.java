6463337006fc9f7217b059b1e42dc5a8104f2c8a
Junhao Wang <junhaowww@outlook.com>
2019-10-06 23:17:02 -0700

    6.8 Convert From Roman to Decimal

--- epi_judge_java/epi/RomanToInteger.java
+++ epi_judge_java/epi/RomanToInteger.java
@@ -5,9 +5,32 @@ public class RomanToInteger {
   @EpiTest(testDataFile = "roman_to_integer.tsv")
 
   public static int romanToInteger(String s) {
-    // TODO - you fill in here.
-    return 0;
+    int n = s.length();
+    int result = 0;
+    for (int i = 0; i < n; ++i) {
+      int curr = getVal(s.charAt(i));
+      int next = (i + 1 < n) ? getVal(s.charAt(i + 1)) : Integer.MIN_VALUE;
+      if (curr < next) {
+        result -= curr;
+      } else {
+        result += curr;
+      }
+    }
+    return result;
   }
+
+  private static int getVal(char ch) {
+    if (ch == 'I') return 1;
+    if (ch == 'V') return 5;
+    if (ch == 'X') return 10;
+    if (ch == 'L') return 50;
+    if (ch == 'C') return 100;
+    if (ch == 'D') return 500;
+    if (ch == 'M') return 1000;
+    throw new IllegalArgumentException("Unsupported Character");
+  }
+
+
 
   public static void main(String[] args) {
     System.exit(
