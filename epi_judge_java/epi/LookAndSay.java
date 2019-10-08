61c6ec2594edca3e113cef55f505da0b6120605d
Junhao Wang <junhaowww@outlook.com>
2019-10-07 22:39:11 -0700

    6.7 The Look-And-Say Problem

--- epi_judge_java/epi/LookAndSay.java
+++ epi_judge_java/epi/LookAndSay.java
@@ -5,8 +5,27 @@ public class LookAndSay {
   @EpiTest(testDataFile = "look_and_say.tsv")
 
   public static String lookAndSay(int n) {
-    // TODO - you fill in here.
-    return "";
+    String prev = "1";
+    for (int i = 1; i < n; ++i) {
+      prev = lookSay(prev);
+    }
+    return prev;
+  }
+
+  private static String lookSay(String s) {
+    StringBuilder sb = new StringBuilder();
+    // go through the string
+    int i = 0;
+    while (i < s.length()) {
+      int num = s.charAt(i) - '0';
+      int count = 0;
+      while (i < s.length() && s.charAt(i) - '0' == num) {
+        count += 1;
+        i += 1;
+      }
+      sb.append(count); sb.append(num);
+    }
+    return sb.toString();
   }
 
   public static void main(String[] args) {
