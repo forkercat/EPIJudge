208725c87a2c3f3903edb76a6416da2930fe30d5
Junhao Wang <junhaowww@outlook.com>
2019-10-07 23:57:54 -0700

    6.6 Reverse All the Words in a Sentence

--- epi_judge_java/epi/ReverseWords.java
+++ epi_judge_java/epi/ReverseWords.java
@@ -5,9 +5,37 @@ import epi.test_framework.TimedExecutor;
 public class ReverseWords {
 
   public static void reverseWords(char[] input) {
-    // TODO - you fill in here.
-    return;
+    int n = input.length;
+    // reverse all
+    reverse(input, 0, n - 1);
+    int i = 0;
+    while (i < n) {
+      int end = i;
+      while (end < n && input[end] != ' ') {
+        end += 1;
+      } // end finally points to ' '
+      reverse(input, i, end - 1);
+      i = end + 1;
+    }
   }
+
+  private static void reverse(char[] charArr, int lo, int hi) {
+    int n = hi - lo + 1;
+    for (int i = 0; i < n / 2; ++i) {
+      swap(charArr, lo + i, hi - i);
+    }
+  }
+
+  private static void swap(char[] charArr, int i, int j) {
+    char temp = charArr[i];
+    charArr[i] = charArr[j];
+    charArr[j] = temp;
+  }
+
+
+
+
+
   @EpiTest(testDataFile = "reverse_words.tsv")
   public static String reverseWordsWrapper(TimedExecutor executor, String s)
       throws Exception {
