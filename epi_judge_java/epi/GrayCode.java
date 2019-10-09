f5f2e5dc552180d2f516401e89f22a201e200d53
Junhao Wang <junhaowww@outlook.com>
2019-10-08 17:25:58 -0700

    15.11 Gray Code

--- epi_judge_java/epi/GrayCode.java
+++ epi_judge_java/epi/GrayCode.java
@@ -1,21 +1,73 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+
+import java.util.*;
+
 public class GrayCode {
 
+  /**
+   * Backtracking
+   */
+  /*
   public static List<Integer> grayCode(int numBits) {
-    // TODO - you fill in here.
-    return Collections.emptyList();
+    List<Integer> result = new ArrayList<>(List.of(0));
+    directedGrayCode(numBits, new HashSet<>(List.of(0)), result);
+    return result;
   }
+
+  private static boolean directedGrayCode(int numBits, Set<Integer> history, List<Integer> result) {
+    // if (result.size() == (int) Math.pow(2, numBits))
+    if (result.size() == (1 << numBits)) { // 1 << numBits equals 2^numBits
+      // compare the first element and the last element
+      return differsByOneBit(result.get(0), result.get(result.size() - 1));
+    }
+    for (int i = 0; i < numBits; ++i) {
+      int prevCode = result.get(result.size() - 1);
+      int candCode = prevCode ^ (1 << i); // xor 001, 010, 100
+      if (!history.contains(candCode)) { // candCode not in history
+        history.add(candCode);
+        result.add(candCode);
+        if (directedGrayCode(numBits, history, result)) {
+          return true; // found
+        }
+        history.remove(candCode);
+        result.remove(result.size() - 1);
+      }
+    }
+    return false;
+  }
+
+   */
+
+
+  public static List<Integer> grayCode(int numBits) {
+    List<Integer> result = new ArrayList<>(List.of(0));
+    for (int i = 0; i < numBits; ++i) {
+      List<Integer> res1 = result;
+      List<Integer> res2 = new ArrayList<>(res1);
+      Collections.reverse(res2);
+      // prepend 1 to res2
+      int prependVal = (1 << i); // if i = 1, we have 10
+      for (int j = 0; j < res2.size(); ++j) {
+        res2.set(j, prependVal + res2.get(j));
+      }
+      // combine
+      res1.addAll(res2);
+    }
+    return result;
+  }
+
+
+
   private static boolean differsByOneBit(int x, int y) {
-    int bitDifference = x ^ y;
-    return bitDifference != 0 && (bitDifference & (bitDifference - 1)) == 0;
+    int xor = x ^ y;
+    // x == y: xor == 0
+    // diff bit > 1: (xor & (xor - 1)) != 0
+    return xor != 0 && (xor & (xor - 1)) == 0;
   }
 
   @EpiTest(testDataFile = "gray_code.tsv")
